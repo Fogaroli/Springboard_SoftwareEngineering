d13505b06d84b0ef35d4954602348d1dc50c4164
Fabricio Ribeiro <fabricio.ribeiro@outlook.com>
2024-05-30 15:02:55 +0100

    adjust template

--- Unit19-Flask/app.py
+++ Unit19-Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, jsonify, make_response
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -67,3 +67,70 @@ pdb.set_trace()
 def jason_content():
     dic = {"key": "value"}
     return jsonify("dic")
+
+
+# ******************************************************** 
+# Cookies
+
+@app.before_request
+def run_before_any_rounte():
+    #This function will run before any other @app route being executed. A way to track and do something before any request to the server.
+    pass
+
+
+request.cookies # This returns a dictionary with the value pairs for each cookie entry.
+# This variable is automatically transferred to the html template, and accessible as request.cookies, no need to send it to the html as variable.
+#We can access as request.cookies.items()
+
+
+# On the HTML template, we can add a sub html using
+{% include "other_page.html" %}
+
+# To add a cookie using flask we need to add it to the reponse object, not to the html content. So we need to first generate the response object, add the cookie and then return the response with the cookie.
+
+@app.route("/")
+def home_page_with_cookie():
+    # return "<p>Return HTML</p>" Thsi is the return body, and flask generates the response from it. (can be a templat,e or whatever)
+    content = "<p>Return HTML</p>"
+    res = make_response(content)
+    res.set_cookie("color", "red")
+    return res
+
+
+# Flask Session
+
+from flask import session
+# We need a secret key in order to use for criptography on the cookie content.
+# Inside the Routes we can user the sessions as a disctionary that will be converted by Flask to Cookies
+
+@app.route(/session)
+def session_demo():
+    session["any_key"] = "Value"
+    # We can use any type of data format here, even lists and other dictionaries.
+    return "Anything, after using the cookey data"
+
+# When using session, the data is available directly on the html template through Jinja, we can just using {{session["anything"]}} in the html
+
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Flask Unit test
+
+# Test as added on a second file, and will look for specifics.
+
+import app # to import the python application "app"
+from unittest import TestCase
+
+class My_Tests(TestCase):
+    def test_testname(self):
+        with app.test_client() as client:
+            res = client.get('/') # or we can use res = client.post('url', data = {data_dic})
+            # for page redirects we can use res = client.get('URL', follow_redirects=True)
+            html = res.get_data(as_text = True)
+
+            # Now set the asserts logic
+            # self.assertEqual()"- res.status_code, 200"
+
+            # To test session: 
+            with client.session_transaction() as change_session:
+                change_session["variable"] = "new_value"
